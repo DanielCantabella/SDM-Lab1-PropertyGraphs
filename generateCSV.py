bc2d85c5c440ad57321ce49d0ea02da51f7815f2
DanielCantabella <daniel.cantabella.cantabella@estudiantat.upc.edu>
2023-03-01 21:20:16 +0100

    sample data and csv from SemanticScholar

---
+++ generateCSV.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import csv
+
+tables=['abstracts', 'authors', 'citations', 'embeddings', 'paper-ids', 'papers', 'publication-venues', 's2orc', 'tldrs']
+for table in tables:
+    df = pd.read_json('./data/samples/'+table+'/'+table+'-sample.jsonl.gz', lines=True, compression='gzip')
+    df.to_csv('./data/csv/SemanticScholar/'+table+'.csv')
+
+
+
+
+
+
+
+
