ffbb73a1818c1ecabb354e96eb5c3a5605b66747
DanielCantabella <daniel.cantabella.cantabella@estudiantat.upc.edu>
2023-03-01 21:28:18 +0100

    Code for generating .csv from sample SemanticScholar data

--- generateCSV.py
+++ generateCSV.py
@@ -5,11 +5,3 @@ tables=['abstracts', 'authors', 'citations', 'embeddings', 'paper-ids', 'papers'
 for table in tables:
     df = pd.read_json('./data/samples/'+table+'/'+table+'-sample.jsonl.gz', lines=True, compression='gzip')
     df.to_csv('./data/csv/SemanticScholar/'+table+'.csv')
-
-
-
-
-
-
-
-
